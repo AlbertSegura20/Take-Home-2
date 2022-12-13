a61ac44cd6e46a2f3e684082515363dec55c29e0
Albert Segura Martinez <seguramartinezal@gmail.com>
2022-12-12 23:28:10 -0400

    first commit

---
+++ src/components/App/App.tsx
@@ -0,0 +1,17 @@
+import type { FC } from "react";
+import "./App.css";
+
+import Agents from "../Agents/Agents";
+import Menubar from "../Menu/Menubar";
+
+
+const App: FC = () => {
+  return (
+    <div className="app">
+        <Menubar/>
+      <Agents />
+    </div>
+  );
+};
+
+export default App;
